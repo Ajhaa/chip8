4f348fdd056328d67b344bd0e7862416f1f1c251
atte <atte.h.1998@gmail.com>
2020-02-02 19:34:07 +0200

    Lots of instructions!

---
+++ src/chip.rs
@@ -0,0 +1,251 @@
+use std::collections::HashMap;
+
+pub struct Chip {
+    opcode: u16,
+
+    pub memory: [u8; 4096],
+    pub V: [u8; 16],
+
+    pub I: u16,
+    pub pc: usize,
+
+    pub gfx: [u8; 64*32],
+
+    delay_timer: u8,
+    sound_timer: u8,
+
+    pub stack: [u16; 16],
+    pub sp: usize,
+}
+
+fn extract_reg_and_byte(opcode: u16) -> (usize, u8) {
+    let value = opcode & 0x00FF;
+    let reg = (opcode & 0x0F00) >> 8;
+
+    return (reg as usize, value as u8);
+}
+
+impl Chip {
+    pub fn new() -> Chip {
+        Chip {
+            opcode: 0,
+            memory: [0; 4096],
+            V: [0; 16],
+            I: 0,
+            pc: 0x200,
+            gfx: [0; 64*32],
+            delay_timer: 0,
+            sound_timer: 0,
+            stack: [0; 16],
+            sp: 0,
+        }
+    }
+
+    pub fn get_opcode(&self) -> u16 {
+        let code = self.memory[self.pc] as u16;
+        code << 8 | self.memory[self.pc + 1] as u16
+    }
+
+    pub fn load_instructions(&mut self, list: &Vec<u16>) {
+        let mut i = 0x200;
+        for inst in list {
+            let p1 = (inst & 0xFF00) >> 8;
+            let p2 = inst & 0x00FF;  
+            self.memory[i] = p1 as u8;
+            self.memory[i+1] = p2 as u8;
+            i += 2;
+        }
+    }
+
+    pub fn reg_dump(&self) {
+        let mut i = 0;
+        for reg in &self.V {
+            println!("V{}: {}", i, reg);
+            i += 1;
+        }
+    }
+
+    pub fn mem_dump(&self, start: usize, end: usize) {
+        for i in start..end {
+            let val = self.memory[i];
+            println!("{:X}: 0x{:X} {}", i, val, val);
+        }
+    }
+
+    pub fn cycle(&mut self) {
+        let opcode = self.get_opcode();
+        match opcode & 0xF000 {
+            0x0000 => {
+                match opcode & 0x000F {
+                    // CLS
+                    0x0000 => {
+                        self.gfx = [0; 64*32]
+                    },
+                    // RET
+                    0x000E => {
+                        self.pc = self.stack[self.sp] as usize;
+                        self.sp -= 1;
+                        return;
+                    },
+                    _ => println!("unknown opcode {}", opcode)
+                }
+            },
+            // JP addr
+            0x1000 => {
+                let new_addr = (opcode & 0x0FFF) as usize;
+                self.pc = new_addr;
+                return;
+            },
+            // CALL addr
+            0x2000 => {
+                let new_pc = (opcode & 0x0FFF) as usize;
+
+                self.sp += 1; 
+
+                self.stack[self.sp] = self.pc as u16;
+                self.pc = new_pc;
+                return;
+            },
+            // SE Vx, byte
+            0x3000 => {
+                let (reg, value) = extract_reg_and_byte(opcode);
+                if self.V[reg] == value {
+                    self.pc += 2;
+                }
+            },
+            // SNE vx, byte
+            0x4000 => {
+                let (reg, value) = extract_reg_and_byte(opcode);
+                if self.V[reg] != value {
+                    self.pc += 2;
+                } 
+            },
+            // SE vx, vy
+            0x5000 => {
+                let reg = ((opcode & 0x0F00) >> 8) as usize;
+                let reg2 = ((opcode & 0x00F0) >> 4) as usize;
+                if self.V[reg] == self.V[reg2] {
+                    self.pc += 2;
+                } 
+            }
+            // LD Vx, byte
+            0x6000 => {
+                let (reg, value) = extract_reg_and_byte(opcode);
+                self.V[reg] = value;
+            },
+            //ADD Vx, byte
+            0x7000 => {
+                let (reg, value) = extract_reg_and_byte(opcode);
+                self.V[reg] = self.V[reg] + value;
+            },
+            0x8000 => {
+                let x = ((opcode & 0x0F00) >> 8) as usize;
+                let y = ((opcode & 0x00F0) >> 4) as usize;
+                match opcode & 0x000F {
+                    // LD Vx, Vy
+                    0x0000 => {
+                        self.V[x] = self.V[y];
+                    },
+                    // OR Vx, Vy
+                    0x0001 => {
+                        self.V[x] |= self.V[y];
+                    },
+                    // AND Vx, Vy
+                    0x0002 => {
+                        self.V[x] &= self.V[y];
+                    },
+                    // XOR Vx, Vy
+                    0x0003 => {
+                        self.V[x] ^= self.V[y];
+                    },
+                    // ADD Vx, Vy
+                    0x0004 => {
+                        let sum = self.V[x] + self.V[y];
+
+                        self.V[x] = sum;
+
+                        if sum < self.V[x] {
+                            self.V[0xF] = 1;
+                        } else {
+                            self.V[0xF] = 0;
+                        }
+                    },
+                    // SUB Vx, Vy
+                    0x0005 => {
+                        let res = self.V[x] - self.V[y];
+                        self.V[x] = res;
+                        if self.V[x] > self.V[y] {
+                            self.V[0xF] = 1;
+                        } else {
+                            self.V[0xF] = 0;
+                        }
+                    },
+                    // SHR Vx
+                    0x0006 => {
+                        if self.V[x] % 2 == 0 {
+                            self.V[0xF] = 0;
+                        } else {
+                            self.V[0xF] = 1;
+                        }
+                        self.V[x] /= 2;
+                    },
+                    // SUBN Vx, Vy
+                    0x0007 => {
+                        let res = self.V[y] - self.V[x];
+                        self.V[x] = res;
+                        if self.V[y] > self.V[x] {
+                            self.V[0xF] = 1;
+                        } else {
+                            self.V[0xF] = 0;
+                        }
+                    },
+                    0x000E => {
+                        if self.V[x] % 2 == 0 {
+                            self.V[0xF] = 0;
+                        } else {
+                            self.V[0xF] = 1;
+                        }
+                        self.V[x] *= 2;
+                    },
+                    _ => ()
+                }
+            },
+            0x9000 => {
+                let reg = ((opcode & 0x0F00) >> 8) as usize;
+                let reg2 = ((opcode & 0x00F0) >> 4) as usize;
+                if self.V[reg] != self.V[reg2] {
+                    self.pc += 2;
+                } 
+            },
+            // LD i, addr
+            0xA000 => {
+                self.I = opcode & 0x0FFF;
+            },
+            // JP V0, addr
+            0xB000 => {
+                let mut new_pc = (opcode & 0x0FFF) as usize;
+                new_pc += self.V[0] as usize;
+                self.pc = new_pc;
+            },
+            0xF000 => {
+                match opcode & 0x00FF {
+                    0x001E => {
+                        let reg = ((opcode & 0x0F00) >> 8) as usize;
+                        self.I += self.V[reg] as u16;
+
+                    },
+                    0x0055 => {
+                        let end = (opcode & 0x0F00) >> 8;
+                        for i in 0..end+1 {
+                            self.memory[(self.I+i) as usize] = self.V[i as usize];
+                        } 
+                    },
+                    _ => ()
+                }
+            },
+            _ => println!("unknown opcode {}", opcode)
+        }
+
+        self.pc += 2;
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,46 @@
+mod chip;
+
+use chip::Chip;
+use std::io;
+
+fn main() -> io::Result<()> {
+    let mut system = Chip::new();
+
+    let fibonacci = vec![
+        0x6501, // LD V5 1 ; add this to I (memory pointer) later
+        0xA400, // LD I 0x400 ; set memory pointer to 0x400
+
+        0x6400, // LD V4 0 ; loop counter
+
+        0x6100, // LD V1 0 ; init fibonacci seq
+        0x6201, // LD V2 1
+
+        0x8320, // LD V3 V2 ; save previous fibonacci number to V3
+        0x8214, // ADD V2 V1 ; calculate next fibonacci number to V2
+        0x8130, // LD V1 V3 ; write previous fibonacci number to V1
+
+        0x7401, // ADD V4 1 ; increment loop counter
+
+        0x8020, // LD V0 V2 ; load latest fibonacci to V0
+        0xF055, // LD [I], 0 ; save V0 to memory pointed in I
+
+        0xF51E, // ADD I V5 ; increment I by 1
+        0x3407, // SE V4 07 ; if loop counter is 7, skip next instruction
+        0x120A, // JP 0x20A ; jump to the sixth instruction
+    ];
+
+
+    system.load_instructions(&fibonacci);
+    
+    while system.pc - 0x200 < fibonacci.len() * 2 {
+        system.cycle();
+    }
+    
+    
+    system.mem_dump(0x400, 0x40A);
+    system.reg_dump();
+
+    Ok(())
+}
+
+
